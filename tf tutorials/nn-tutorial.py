1226fd6c90c8e319f98964af27021f289f2190be
therealjtgill <jtgill@email.arizona.edu>
2017-02-16 00:17:11 -0700

    Comments added
    How the dynamic_rnn()'s output needs to be reshaped so that it can be multiplied by a weight matrix.

--- tf tutorials/nn-tutorial.py
+++ tf tutorials/nn-tutorial.py
@@ -10,25 +10,34 @@ batch_size = 32
 weights = {'out':tf.Variable(tf.random_normal([num_chars, num_hidden_units]))}
 biases = {'out': tf.Variable(tf.random_normal([num_chars]))}
 
-
 # Declaring this as a placeholder means that it must be fed with data 
 # at execution time.
 # This means that its value has to be specified using the feed_dict = {}
 # argument inside of Session.run(), Tensor.eval(), or Operation.run()
-# The value that you feed it is most likely going to be a numpy array.
+# The value that you feed it is going to be a numpy array.
 feed_x = tf.placeholder(dtype=tf.float32, shape=(None, num_chars))
 feed_y = tf.placeholder(dtype=tf.float32, shape=(None, num_chars))
 
-def rnn(input_vector, weights, biases):
+def rnn(input_vector):
 	# The cell state and the hidden state are vectors with the same number
 	# of components. This is usually coupled with an op that constructs the
 	# actual network - rnn.static_rnn, dynamic_rnn, etc.
 	lstm_cell = rnn.BasicLSTMCell(num_units=num_hidden_units, forget_bias=1.0, state_is_tuple=True)
-	
-	# Outputs should have dimensions of [num_unrolls, num_hidden_units]
+
+	# 'output' should have dimensions of [batch_size, num_unrolls, num_hidden_units]
 	# Since the RNN was defined as being dynamic, the amount of layer unrolling
 	# can change from batch to batch.
-	outputs, states = tf.nn.dynamic_rnn(cell=lstm_cell, inputs=input_vector,dtype=tf.float32)
+	output, states = tf.nn.dynamic_rnn(cell=lstm_cell, inputs=input_vector,dtype=tf.float32)
 
+	return output
 
-	return outputs
+output = rnn(feed_x)
+
+# Split the output tensor of size num_unrolls*batch_size*num_hidden_units into
+# 'batch_size' copies of vectors of size num_unrolls*num_hidden_units.
+outputs = tf.unstack(output, axis=0)
+
+# Need to reshape the 'outputs' to have a shape of [1, num_hidden_units], multiply
+# it by the weight matrix, and add the bias terms.
+# You'll use the local field (activation function argument?) as an argument to
+# the gradient descent optimizer. Need to fidget with this part.
