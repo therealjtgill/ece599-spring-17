f579b3bf309e5158c8079c6017a04bb10d2fcda1
therealjtgill <jtgill@email.arizona.edu>
2017-02-17 01:54:48 -0700

    Training/execution ops added
    Need to add a method to scrape batches out of a text file

--- tf tutorials/nn-tutorial.py
+++ tf tutorials/nn-tutorial.py
@@ -1,13 +1,21 @@
 import tensorflow as tf
 from tensorflow.contrib import rnn
 
+# Need to gather the training samples so the max number of training steps can
+# be defined.
+
 num_chars = 28
 num_timesteps = 20
 num_hidden_units = 200
 learning_rate = 0.001
 batch_size = 32
+max_epochs = 30
+max_steps = max_epochs * batch_size / num_training_samples
+sample_step = 100
+# The maximum number of steps you go through is 
+#   max_epochs * batch_size / num_training_samples
 
-weights = {'out':tf.Variable(tf.random_normal([num_chars, num_hidden_units]))}
+weights = {'out':tf.Variable(tf.random_normal([num_hidden_units, num_chars]))}
 biases = {'out': tf.Variable(tf.random_normal([num_chars]))}
 
 # Declaring this as a placeholder means that it must be fed with data 
@@ -18,7 +26,7 @@ biases = {'out': tf.Variable(tf.random_normal([num_chars]))}
 feed_x = tf.placeholder(dtype=tf.float32, shape=(None, num_chars))
 feed_y = tf.placeholder(dtype=tf.float32, shape=(None, num_chars))
 
-def rnn(input_vector):
+def rnn(input_vector, w, b):
 	# The cell state and the hidden state are vectors with the same number
 	# of components. This is usually coupled with an op that constructs the
 	# actual network - rnn.static_rnn, dynamic_rnn, etc.
@@ -29,15 +37,51 @@ def rnn(input_vector):
 	# can change from batch to batch.
 	output, states = tf.nn.dynamic_rnn(cell=lstm_cell, inputs=input_vector,dtype=tf.float32)
 
-	return output
+	local_field = tf.matmul(tf.reshape(outputs, [-1, num_hidden_units]), w) + b
 
-output = rnn(feed_x)
+	return local_field
 
-# Split the output tensor of size num_unrolls*batch_size*num_hidden_units into
-# 'batch_size' copies of vectors of size num_unrolls*num_hidden_units.
-outputs = tf.unstack(output, axis=0)
+def get_next_batch(batch_size):
+	
 
-# Need to reshape the 'outputs' to have a shape of [1, num_hidden_units], multiply
-# it by the weight matrix, and add the bias terms.
-# You'll use the local field (activation function argument?) as an argument to
-# the gradient descent optimizer. Need to fidget with this part.
+local_field = rnn(feed_x, weights['out'], biases['out'])
+
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=local_field, labels=feed_y))
+
+softmax_output = tf.nn.softmax(local_field)
+
+optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
+
+init = tf.global_variables_initializer()
+
+with tf.Session() as sess:
+	# Initialize all of the variables we've declared to their default values.
+	# In this context, you initialize the weights and biases to random
+	# normal values.
+	sess.run(init)
+
+	# Recall that one epoch is a forward pass and backward pass for every 
+	# training sample.
+	step = 1
+	
+	# The total number of steps is the number of batches that have been passed
+	# through for training. So for one epoch there are
+	#   num_training_samples/batch_size
+	# steps.
+
+	while step < max_steps:
+		# The training batch has to have specific dimensions:
+		#  (batch_size, num_unrolls, num_inputs)
+		# num_unrolls - number of characters in the sequence
+		# num_inputs - number of 'items' in the training vocabulary (28)
+		# Since we're building a language model, the training_batch is also the
+		# desired outputs.
+		train_batch = get_next_batch(batch_size)
+
+		# Note that the elements of the feed dictionary are the placeholders
+		# defined earlier in the program.
+		sess.run(optimizer, feed_dict={feed_x: train_batch, feed_y: train_batch})
+
+		if steps % sample_step == 0:
+
+			print(sess.run(softmax_output, feed_dict={feed_x: train_batch}))
