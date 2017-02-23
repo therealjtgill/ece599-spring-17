a25c24803598f93774c25bbc580c1279603f538d
therealjtgill <jtgill@email.arizona.edu>
2017-02-22 20:07:52 -0700

    Load feature finally added
    Network can now run with an arbitrary-length input string.

--- tf tutorials/rnn-tutorial-with-load.py
+++ tf tutorials/rnn-tutorial-with-load.py
@@ -14,21 +14,33 @@ all_data = all_data.lower()
 
 # set() - returns a set object of all of the unique items in a list. In this
 #   case, the list is a sequence of characters comprising the entirety of the
-#   text contained in 'all_data'
+#   text contained in 'all_data'.
 # The list() keyword explicitly converts the set into a list. The locations of
 # elements of the char_vocab can now be translated into one-hot encodings for
 # training and testing.
 char_vocab = list(set(all_data))
 
+# num_chars - Number of unique characters used in the training set.
+# num_timesteps - Number of unrollings to do for training data. (Is this used?)
+# num_hidden_units - Number of hidden neurons in the LSTM cell.
+# learning_rate - Well...
+# batch_size - The number of training data vectors to feed to the network at
+#   a time.
+# batches_per_epoch - Number of batches that will be considered one epoch's 
+#   worth of training.
+# max_epochs - Total number of training loops to be executed.
+# max_steps - Total number of batch passes to the network.
+# sample_step_percentage - Sample output from training will be displayed at
+#   every integer multiple of this percentage during the training steps
 num_chars = len(char_vocab)
 num_timesteps = 50
 num_hidden_units = 200
 learning_rate = 0.001
-batch_size = 32 # Number of character sequences in a batch
+batch_size = 32 
 batches_per_epoch = 500
 max_epochs = 30
 max_steps = max_epochs * batches_per_epoch
-sample_step = 200
+sample_step_percentage = .01
 
 def char_to_vector(char):
 	char = char.lower()
@@ -42,14 +54,18 @@ def vector_to_char(vector):
 	#print("max index", max_index[0].tolist()[0])
 	return char_vocab[max_index[0].tolist()[0]]
 
-# Given the number of timesteps, batch size, and the batch sequence, return matrices
-# of training and testing data. Note that you're extracting 'num_timesteps' number of
-# characters from the training set, 'batch_size' number of sequential sets of characters,
-# and each character will be broken down into a 'num_chars' length one-hot encoded
-# vector.
-# The order of these indices *matters* because the input to the LSTM requires input
-# data in a certain format.
 def get_next_batch(num_timesteps, batch_size, batch_number):
+	'''
+	Given the number of timesteps, batch size, and the batch sequence, return matrices
+	of training and testing data. Note that you're extracting 'num_timesteps' number of
+	characters from the training set, 'batch_size' number of sequential sets of characters,
+	and each character will be broken down into a 'num_chars' length one-hot encoded
+	vector.
+	The order of these indices *matters* because the input to the LSTM requires input
+	data in a certain format.
+	The next modification of this method should be allowing for random sampling from the
+	data set (without replacement).
+	'''
 	start_index = batch_number*batch_size*num_timesteps
 
 	input_matrix = np.zeros((batch_size, num_timesteps, num_chars))
@@ -60,37 +76,33 @@ def get_next_batch(num_timesteps, batch_size, batch_number):
 		input_substring = all_data[index:index + num_timesteps]
 		output_substring = all_data[index+1:index+num_timesteps+1]
 
-		'''
-		if i == 1:
-			print("samples of input and output")
-			print("in:",input_substring)
-			print("out:",output_substring)
-		'''
-
 		for j in range(len(input_substring)):
 			input_matrix[i,j,:] = char_to_vector(input_substring[j])
 			output_matrix[i,j,:] = char_to_vector(output_substring[j])
 
 	return input_matrix, output_matrix
 
-def string_to_tensor(input_string):
-	input_matrix = np.zeros((1, len(input_string), num_chars))
-	for j in range(len(input_string)):
-		input_matrix[1,j,:] = char_to_vector(input_string[j])
-	return input_matrix
+def string_to_tensor(string_):
+	'''
+	Converts a string into a tensor (numpy array). This is intended for running the
+	network on a sequence of characters.
+	The output will have the shape
+	[1 string_length vocabulary_size]
+	'''
+	tensor = np.zeros((1, len(string_), num_chars))
+	for j in range(len(string_)):
+		tensor[1,j,:] = char_to_vector(string_[j])
+	return tensor
 
 
-# The maximum number of steps you go through is 
-#   max_epochs * batch_size / num_training_samples
-
-
-# Converting this to a class so that object properties can be taken advantage of.
+# Converted this process from a regular method to a class so that object properties
+# can be taken advantage of.
 class RNN(object):
 
-	def __init__(self, learning_rate, session):
+	def __init__(self, learning_rate, session, scope_name):
 		self.session = session
 
-		with tf.variable_scope('stuff'):
+		with tf.variable_scope(scope_name):
 			self.weights = tf.Variable(tf.random_normal([num_hidden_units, num_chars]))
 			self.biases = tf.Variable(tf.random_normal([num_chars]))
 
@@ -122,9 +134,14 @@ class RNN(object):
 			# Since the RNN was defined as being dynamic, the amount of layer unrolling
 			# can change from batch to batch.
 			self.outputs, self.last_lstm_state = tf.nn.dynamic_rnn(cell=self.lstm_cell, inputs=self.feed_x, initial_state=self.rnn_tuple_state, dtype=tf.float32)
-			print(self.outputs)
+			print("output from dynamic rnn", self.outputs)
 
+			# The local field of the softmax output (argument of the softmax).
+			# This should return a matrix.
+			# There's a bias for each output node, and the outputs of the dynamic_rnn are for
+			# each timestep, so the output *should* be a matrix of outputs at each timestep.
 			self.local_field = tf.matmul(tf.reshape(self.outputs, [-1, num_hidden_units]), self.weights) + self.biases
+			print("size of local field", self.local_field)
 
 			# What we're ultimately reducing.
 			# This is an operation on the tensorflow graph that contains the 'local_field' object
@@ -138,8 +155,7 @@ class RNN(object):
 			self.train_operation = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(self.cost)
 
 	def train(self, batch_x, batch_y):
-		# Need to add state_c and need to cut the zero state down... maybe?
-		#print("lstm zero state", self.lstm_zero_state[0], self.session.run(self.lstm_zero_state[0]))
+		
 		zero_state_r = self.session.run(self.lstm_zero_state[0])
 		zero_state_c = self.session.run(self.lstm_zero_state[1])
 		self.session.run(self.train_operation, feed_dict={self.feed_x:batch_x, self.feed_y:batch_y, self.state_r:zero_state_r, self.state_c:zero_state_c})
@@ -150,50 +166,54 @@ class RNN(object):
 		the input tensor, 'x', to the network.
 		'''
 		test_output = ""
-		zero_state_r = self.session.run(self.lstm_zero_state[0])
-		zero_state_c = self.session.run(self.lstm_zero_state[1])
-
-		softmax_out, lstm_out, lstm_state_out = self.session.run([self.softmax_output, self.outputs, self.last_lstm_state], feed_dict={self.feed_x:x, self.state_r:zero_state_r, self.state_c:zero_state_c})
+		#zero_state_r = self.session.run(self.lstm_zero_state[0])
+		#zero_state_c = self.session.run(self.lstm_zero_state[1])
+		zero_state_r = np.zeros((x.shape[0], num_hidden_units))
+		zero_state_c = np.zeros((x.shape[0], num_hidden_units))
+		feed_dict={self.feed_x:x, self.state_r:zero_state_r, self.state_c:zero_state_c}
+		softmax_out, lstm_out, lstm_state_out = self.session.run([self.softmax_output, self.outputs, self.last_lstm_state], feed_dict=feed_dict)
 		print("softmax_out shape", softmax_out.shape)
 		for j in range(softmax_out.shape[0]):
 			test_output += vector_to_char(softmax_out[j])
 
 		for i in range(num_steps):
-			lstm_in = lstm_out
-			lstm_state_in = lstm_state_out
-			softmax_out, lstm_out, lstm_state_out = self.session.run([self.softmax_output, self.outputs, self.last_lstm_state], feed_dict={self.feed_x:lstm_in, self.state_r:lstm_state_in})
+			lstm_in = np.zeros(softmax_out.shape)
+			lstm_in[np.where(softmax_out==np.amax(softmax_out))] = 1.0
+			lstm_state_r_in = lstm_state_out[0]
+			lstm_state_c_in = lstm_state_out[1]
+			feed_dict={self.feed_x:lstm_in, self.state_r:lstm_state_r_in, self.state_c:lstm_state_c_in}
+			softmax_out, lstm_out, lstm_state_out = self.session.run([self.softmax_output, self.outputs, self.last_lstm_state], feed_dict=feed_dict)
 			for j in range(lstm_out.shape[0]):
 				test_output += vector_to_char(softmax_out[j])
 
 		return test_output
 
-
-	
-#local_field, final_state = rnn(feed_x, weights['out'], biases['out'])
-
+# Start tf session. This is passed to the RNN class.
 sess = tf.Session()
 
-network = RNN(learning_rate, sess)
+network = RNN(learning_rate, sess, 'stuff')
+
 # Initialize all of the variables inside of the 'sess' tensorflow session.
 sess.run(tf.global_variables_initializer())
 
 # Declare a saver for the ops on the graph.
 saver = tf.train.Saver()
 
-
-
 #if os.path.isfile('checkpoint'):
 if False:
-	saver.restore(sess, os.getcwd() + "\\lstmsmall")
+	saver.restore(sess, os.path.join(os.getcwd(), "lstmsmall"))
 
 	print(network.run(string_to_tensor('random stuff')))
 
 else:
-	# Recall that one epoch is a forward pass and backward pass for every 
-	# training sample.
+	# Recall that one epoch is a forward pass and backward pass for all 
+	# training data.
 	step = 1
 
-	print("maximum number of steps:", max_steps)
+	#print("maximum number of steps:", max_steps)
+	# Retrieve the list of trainable variables. The goal is to save these
+	# weights as a grayscale PNG to show how the values evolve over time.
+	trainable_vars = tf.trainable_variables()
 	
 	# The total number of steps is the number of batches that have been passed
 	# through for training. So for one epoch there are
@@ -201,9 +221,9 @@ else:
 	# steps.
 	while step < max_steps:
 		# The training batch has to have specific dimensions:
-		#  (batch_size, num_unrolls, num_inputs)
+		#   (batch_size, num_unrolls, vocab_length)
 		# num_unrolls - number of characters in the sequence
-		# num_inputs - number of 'items' in the training vocabulary (28)
+		# vocab_length - number of unique items in the training vocabulary
 		# Since we're building a language model, the training_batch is also the
 		# desired outputs, except the desired outputs are shifted by one word.
 		train_batch_inputs, desired_batch_outputs = get_next_batch(num_timesteps, batch_size, step % batches_per_epoch)
@@ -211,25 +231,15 @@ else:
 		# Note that the elements of the feed dictionary are the placeholders
 		# defined earlier in the program.
 		network.train(train_batch_inputs, desired_batch_outputs)
-
-		if step % sample_step == 0:
+		print(step)
+		if step % int(sample_step_percentage*float(max_steps)) == 0:
 			print("-----------------------------")
 			print("current step", step)
 
-			#training_output = sess.run(softmax_output, feed_dict={feed_x: train_batch_inputs})
 			training_output = network.run(train_batch_inputs, num_steps=0)
 			print("training output:\n", training_output)
-
-			#print(training_output.shape)
-			# Just need to collect the max outputs of the softmax layer and
-			# convert the indices back to characters. Hopefully that shit looks
-			# decent.
-			#phrase = ""
-			#for i in range(training_output.shape[0]):
-			#	phrase += vector_to_char(training_output[i])
-
-			#print(phrase)
+			#print("local field:\n", sess.run(network.local_field))
 
 		step += 1
 
-	saver.save(sess, os.getcwd() + "\\lstmsmall")
+	saver.save(sess, os.path.join(os.getcwd(), "lstmsmall"))
